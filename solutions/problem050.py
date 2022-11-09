6534e144f34ba47a8b4c78a60038e865dc448008
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2022-11-09 13:49:30 +0100

    WIP 50

--- solutions/problem050.py
+++ solutions/problem050.py
@@ -1,68 +1,38 @@
-from itertools import product
 import numpy as np
-from tqdm import tqdm
-
+import pandas as pd
 import utils.number_theory as nt
 
 
-# def compute_sums3(primes) -> np.array:
-#     matrix = np.zeros((len(primes), len(primes)), dtype=np.int64)
-#     for row, col in tqdm(product(range(len(primes)), repeat=2), total=len(primes) ** 2):
-#         if row >= col:
-#             if row == col:
-#                 value = primes[row]
-#             else:
-#                 value = primes[row] + matrix[row - 1, col]
-#             matrix[row, col] = value
-#     return matrix
-#
-#
-# def compute_sums2(primes) -> np.array:
-#     primes = np.array(primes)
-#     cumsum = np.cumsum(primes)
-#     matrix = np.empty((len(primes), len(primes)), dtype=np.int64)
-#     matrix[:, 0] = cumsum
-#     for col in tqdm(range(1, len(primes))):
-#         matrix[:, col] = np.clip(matrix[:, col - 1] - primes[col - 1], 0, np.iinfo(np.int64).max)
-#     return matrix
-
-
-def compute_sums(primes) -> np.array:
+def compute_sums(primes: np.array) -> np.array:
     cumsum = np.cumsum(primes)
-    matrix = cumsum.reshape(-1, 1) - np.concatenate(([0], cumsum[:-1]))
-    return np.tril(matrix)
+    return cumsum - np.concatenate(([0], cumsum[:-1])).reshape(-1, 1)
 
 
-def generate_len_fn(matrix):
-    def len_fn(value):
-        ranges = np.argwhere(matrix == value)
-        lengths = np.diff(ranges, axis=1)
-        return -np.min(lengths)
-    return np.vectorize(len_fn)
+def unravel(matrix: np.array) -> np.array:
+    span = np.arange(matrix.shape[0])
+    diffs = span - span.reshape(-1, 1) + 1
+    return np.vstack((matrix.ravel(), diffs.ravel())).T
 
 
 def problem50():
-    limit = 1000
+    limit = 1000000
     primes = np.array(nt.sieve_of_atkin(limit))
     print("Done sieve")
     sums = compute_sums(primes)
-    print("Done matrix")
-    base = sums.flatten()
-    print("Done flatten")
-    base = base[base % 2 == 1]
-    print("Done filter")
-    base = np.unique(base)
-    print("Done unique")
-    sum_primes = np.intersect1d(base, primes, assume_unique=True)
-    print("Done intersect")
-    fn = generate_len_fn(sums)
-    print("Done generate")
-    lenghts = fn(sum_primes)
-    print("Done lengths")
-    return sum_primes[np.argmax(lenghts)], np.max(lenghts)
-    # prime_coordinates = np.argwhere(np.isin(sums, primes))
-    # print("Done coordinates")
-    # lenghts = np.diff(prime_coordinates, axis=1)
-    # print("Done diff")
-    # row, col = prime_coordinates[np.argmin(lenghts)]
-    # return sums[row, col]
+    print("Done sums")
+    sums = unravel(sums)
+    print("Done unravel")
+    sums = sums[sums[:, 1] > 21]
+    print("Done len filter")
+    sums = sums[sums[:, 0] % 2 == 1]
+    print("Done even filter")
+    sums = sums[sums[:, 0] < 1000000]
+    print("Done cap filter")
+    # sums = pd.DataFrame(sums, columns=["value", "terms"])
+    # sums = sums.groupby("value", as_index=False).max()
+    # print("Done grouping")
+    # sums = sums.loc[sums.value.isin(primes)]
+    # return sums.iloc[sums.terms.argmax()]
+    sums = sums[np.isin(sums[:, 0], primes)]
+    print("Done prime filter")
+    return sums[np.argmax(sums[:, 1])]
