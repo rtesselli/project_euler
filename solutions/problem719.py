739bef34cf73bd0768a7593048e4409faad8ad3b
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2022-11-10 15:12:19 +0100

    Solve 719

--- solutions/problem719.py
+++ solutions/problem719.py
@@ -1,12 +1,27 @@
-import math
 from tqdm import tqdm
-from more_itertools import partitions
-from utils.series import squares
+import math
 
 
 def s_number(root, value: int) -> bool:
-    digits = str(value)
-    return any(sum(int("".join(group)) for group in partition) == root for partition in partitions(digits))
+    def is_s_number(residual: int, digits: str) -> bool:
+        if not digits:
+            return residual == 0
+        if all(d == '0' for d in digits):
+            return residual == 0
+        if residual <= 0:
+            return False
+        idx = 1
+        n = int(digits[:idx])
+        found = False
+        while not found and n <= residual and idx <= len(digits):
+            found = is_s_number(residual - n, digits[idx:])
+            idx += 1
+            n = int(digits[:idx])
+        return found
+
+    if root <= 1:
+        return False
+    return is_s_number(root, str(value))
 
 
 def problem719():
@@ -16,4 +31,4 @@ def problem719():
         n_square = n ** 2
         if s_number(n, n_square):
             s_sum += n_square
-    return s_sum - 1
+    return s_sum
