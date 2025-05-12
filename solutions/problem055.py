8708dd4d8cd503f47d1457bb857889fc6ec74614
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2025-05-12 16:51:42 +0200

    Solve 55

---
+++ solutions/problem055.py
@@ -0,0 +1,18 @@
+from utils.text import is_palindrome
+
+def is_lychrel(n: int) -> bool:
+    for _ in range(50):
+        n_reversed = int(str(n)[::-1])
+        summed = n + n_reversed
+        if is_palindrome(str(summed)):
+            return False
+        n = summed
+    return True
+
+
+def problem55():
+    lychrels = [n for n in range(1, 10000) if is_lychrel(n)]
+    return len(lychrels)
+
+if __name__ == '__main__':
+    print(is_lychrel(4994))
